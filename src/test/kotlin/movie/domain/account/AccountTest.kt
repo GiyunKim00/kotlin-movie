2d6e4b1472db3d37aaf0373c947e45bdb03c97a6
gykim_kr <k_bright_st@pusan.ac.kr>
2026-04-14 13:15:26 +0900

    fix: Account 도메인 로직 검증 테스트 코드 추가

--- src/test/kotlin/movie/domain/account/AccountTest.kt
+++ src/test/kotlin/movie/domain/account/AccountTest.kt
@@ -1,7 +1,8 @@
 package movie.domain.account
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class AccountTest {
     @Test
@@ -9,6 +10,40 @@ class AccountTest {
         val givenPointAmount = 5_000
         val account = Account(Point(givenPointAmount))
 
-        assertEquals(givenPointAmount, account.point.amount)
+        assertThat(account.point.amount).isEqualTo(givenPointAmount)
+    }
+
+    @Test
+    fun `포인트를 사용하면 해당 금액만큼 차감된다`() {
+        // given
+        val account = Account(Point(2000))
+
+        // when
+        account.useMyPoint(1000)
+
+        // then
+        assertThat(account.point.amount).isEqualTo(1000)
+    }
+
+    @Test
+    fun `보유 포인트보다 많이 사용하면 예외가 발생한다`() {
+        // given
+        val account = Account(Point(1000))
+
+        // when & then
+        assertThrows<IllegalArgumentException> {
+            account.useMyPoint(2000)
+        }
+    }
+
+    @Test
+    fun `음수 포인트를 사용하려고 하면 예외가 발생한다`() {
+        // given
+        val account = Account(Point(1000))
+
+        // when & then
+        assertThrows<IllegalArgumentException> {
+            account.useMyPoint(-1000)
+        }
     }
 }
